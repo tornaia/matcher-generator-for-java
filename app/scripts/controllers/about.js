65351bc0e4496bc3fb1d1acfd754dcd5c68fbc16
Tornai Andras <tornai.andras.ede@gmail.com>
2015-10-14 15:05:44 +0200

    inital import

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matcherGeneratorForJavaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the matcherGeneratorForJavaApp
+ */
+angular.module('matcherGeneratorForJavaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,237 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matcherGeneratorForJavaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the matcherGeneratorForJavaApp
+ */
+angular.module('matcherGeneratorForJavaApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+
+	var TAB = '\t';
+  
+	$scope.sourceText = 'package js.java.parser;\n\npublic class HelloWorld {\n\n\tprivate long oneProperty;\n\n\tprivate Integer twoProperty;\n\n\tpublic long getOneProperty() {\n\t\treturn oneProperty;\n\t}\n\n\tpublic Integer getTwoProperty() {\n\t\treturn twoProperty;\n\t}\n\n\tpublic void setOneProperty(long oneProperty) {\n\t\tthis.oneProperty = oneProperty;\n\t}\n}';
+
+    $scope.convert = function() {
+		function upperCaseFirstChar(input) {
+			return input.substring(0,1).toUpperCase()+input.substring(1);
+		};
+		
+		function lowerCaseFirstChar(input) {
+			return input.substring(0,1).toLowerCase()+input.substring(1);
+		};
+		
+		function convertPrimitiveTypeToBoxedType(type) {
+			if (type === 'int') {
+				return 'Integer';
+			} else if (type === 'char') {
+				return 'Character';
+			}
+			return upperCaseFirstChar(type);
+		}
+		
+		var classAsJson = JavaParser.parse($scope.sourceText);
+		
+		/*
+			package js.java.parser;
+		*/
+		var sectionPackage = (function () {
+									var packages = [];
+									function calculate(obj) {
+										for (var property in obj) {
+											if (obj.hasOwnProperty(property)) {
+												if (property === 'identifier') {
+													packages.push(obj[property]);
+												}
+												if (typeof obj[property] == "object") {
+													calculate(obj[property]);
+												}
+											}
+										}
+									}
+									calculate(classAsJson['package']);
+									return packages.length == 0 ? '' : 'package ' + packages.join('.') + ';';
+								})();
+					
+		/*
+			import org.hamcrest.Matcher;
+			import org.hamcrest.core.IsAnything;
+
+			import hu.ipsystems.matcher.KipTypeSafeDiagnosingMatcher;
+		*/					
+		var sectionImports = 'import static org.hamcrest.Matchers.is;\n\nimport org.hamcrest.Description;\nimport org.hamcrest.Matcher;\nimport org.hamcrest.core.IsAnything;\n\nimport hu.ipsystems.matcher.KipTypeSafeDiagnosingMatcher;';
+					
+		/*
+			public class HelloWorldMatcher extends KipTypeSafeDiagnosingMatcher<HelloWorld> {
+		*/					
+		var sectionClassSignatureOpen = (function () {
+									var baseClassName = classAsJson.types[0].name.identifier;
+									return 'public class ' + baseClassName + 'Matcher extends KipTypeSafeDiagnosingMatcher<' + baseClassName + '> {';
+								})();
+						
+
+		/*
+			private Matcher<Long> value = new IsAnything<>();
+		*/
+		var sectionFields = (function () {
+									var fields = '';
+									var bodyDeclarations = classAsJson.types[0].bodyDeclarations;
+									for (var bodyDeclarationIdx in bodyDeclarations) {
+										var bodyDeclaration = bodyDeclarations[bodyDeclarationIdx];
+										var node = bodyDeclaration.node;
+										
+										var isMethodDeclaration = node === 'MethodDeclaration';
+										if (!isMethodDeclaration) {
+											continue;
+										}
+										
+										var methodIdentifier = bodyDeclaration.name.identifier;
+										var isGetterMethod = methodIdentifier.indexOf('get') != -1;
+										if (!isGetterMethod) {
+											continue;
+										}
+										var variableName = lowerCaseFirstChar(methodIdentifier.substr(3));
+										
+										var matcherClass = undefined;
+										var isPrimitiveType = bodyDeclaration.returnType2.primitiveTypeCode != undefined;
+										if (isPrimitiveType) {
+											matcherClass = convertPrimitiveTypeToBoxedType(bodyDeclaration.returnType2.primitiveTypeCode);
+										} else {
+											matcherClass = bodyDeclaration.returnType2.name.identifier;
+										}
+										bodyDeclaration.returnType2.primitiveTypeCode
+										fields += TAB + 'private Matcher<' + matcherClass + '> ' + variableName + ' = new IsAnything<>();\n\n'
+									}
+									
+									return fields;
+								})();
+
+		/*
+			public BackhaulLineMatcher lineNumber(Integer lineNumber) {
+				this.lineNumber = is(lineNumber);
+			return this;
+		*/
+		var sectionBuilderMethods = (function () {
+									var fields = '';
+									var bodyDeclarations = classAsJson.types[0].bodyDeclarations;
+									for (var bodyDeclarationIdx in bodyDeclarations) {
+										var bodyDeclaration = bodyDeclarations[bodyDeclarationIdx];
+										var node = bodyDeclaration.node;
+										
+										var isMethodDeclaration = node === 'MethodDeclaration';
+										if (!isMethodDeclaration) {
+											continue;
+										}
+										
+										var methodIdentifier = bodyDeclaration.name.identifier;
+										var isGetterMethod = methodIdentifier.indexOf('get') != -1;
+										if (!isGetterMethod) {
+											continue;
+										}
+										var builderReturnType = classAsJson.types[0].name.identifier;
+										var variableName = lowerCaseFirstChar(methodIdentifier.substr(3));
+										
+										var matcherClass = undefined;
+										var isPrimitiveType = bodyDeclaration.returnType2.primitiveTypeCode != undefined;
+										if (isPrimitiveType) {
+											matcherClass = convertPrimitiveTypeToBoxedType(bodyDeclaration.returnType2.primitiveTypeCode);
+										} else {
+											matcherClass = bodyDeclaration.returnType2.name.identifier;
+										}
+										
+										fields += TAB + 'public ' + builderReturnType + 'Matcher ' + variableName + '(' + matcherClass + ' ' + variableName + ') {\n' + TAB + TAB + 'this.' + variableName + ' = is(' + variableName + ');\n' + TAB + TAB + 'return this;\n' + TAB + '}\n\n';
+									}
+									
+									return fields;
+								})();
+								
+		/*
+			@Override
+			protected boolean matchesSafely(BackhaulLine item, Description mismatchDescription) {
+				return matches(lineNumber, item.getLineNumber(), "lineNumber erteke: ", mismatchDescription) &&
+						matches(networkPoint, item.getNetworkPoint(), "networkPointErteke: ", mismatchDescription) &&
+						matches(shipperPair, item.getShipperPair(), "shipperPair erteke: ", mismatchDescription) &&
+						matches(hourlyQuantity, item.getHourlyQuantity(), "hourlyQUantity erteke: ", mismatchDescription);
+			}
+		*/
+		var sectionMatchesSafely = (function () {
+									var baseClassName = classAsJson.types[0].name.identifier;
+									var matchesSafelyPrefix = TAB + '@Override\n' + TAB + 'protected boolean matchesSafely(' + baseClassName + ' item, Description mismatchDescription) {\n' + TAB + TAB + 'return ';
+									
+									var bodyDeclarations = classAsJson.types[0].bodyDeclarations;
+									var matchesSafelyBody = '';
+									for (var bodyDeclarationIdx in bodyDeclarations) {
+										var bodyDeclaration = bodyDeclarations[bodyDeclarationIdx];
+										var node = bodyDeclaration.node;
+										
+										var isMethodDeclaration = node === 'MethodDeclaration';
+										if (!isMethodDeclaration) {
+											continue;
+										}
+										
+										var methodIdentifier = bodyDeclaration.name.identifier;
+										var isGetterMethod = methodIdentifier.indexOf('get') != -1;
+										if (!isGetterMethod) {
+											continue;
+										}
+										var variableName = lowerCaseFirstChar(methodIdentifier.substr(3));
+										if (matchesSafelyBody.length !== 0) {
+											matchesSafelyBody += ' &&\n' + TAB + TAB;
+										}
+										matchesSafelyBody += 'matches(' + variableName + ', item.' + methodIdentifier + '(), "' + variableName + ' erteke: ", mismatchDescription)';
+									}
+									
+									var matchesSafelyPostFix = ';\n' + TAB + '}';
+									return matchesSafelyPrefix + matchesSafelyBody + matchesSafelyPostFix;
+								})();
+								
+		/*
+			@Override
+			public void describeTo(Description description) {
+				description.appendText(BackhaulLine.class.getSimpleName())
+					.appendText(", amiben lineNumber: ").appendDescriptionOf(lineNumber)
+					.appendText(", networkPoint: ").appendDescriptionOf(networkPoint)
+					.appendText(", shipperPair: ").appendDescriptionOf(shipperPair)
+					.appendText(", hourlyQuantity: ").appendDescriptionOf(hourlyQuantity);
+			}
+		*/
+		var sectionDescribeTo = (function () {
+									var baseClassName = classAsJson.types[0].name.identifier;
+									var describeToPrefix = TAB + '@Override\n' + TAB + 'public void describeTo(Description description) {\n' + TAB + TAB + 'description.appendText(' + baseClassName + '.class.getSimpleName())\n';
+									
+									var bodyDeclarations = classAsJson.types[0].bodyDeclarations;
+									var describeToBody = '';
+									for (var bodyDeclarationIdx in bodyDeclarations) {
+										var bodyDeclaration = bodyDeclarations[bodyDeclarationIdx];
+										var node = bodyDeclaration.node;
+										
+										var isMethodDeclaration = node === 'MethodDeclaration';
+										if (!isMethodDeclaration) {
+											continue;
+										}
+										
+										var methodIdentifier = bodyDeclaration.name.identifier;
+										var isGetterMethod = methodIdentifier.indexOf('get') != -1;
+										if (!isGetterMethod) {
+											continue;
+										}
+										var variableName = lowerCaseFirstChar(methodIdentifier.substr(3));
+										if (describeToBody !== '') {
+											describeToBody += '\n';
+										}
+										describeToBody += TAB + TAB + '.appendText(", ' + variableName + ': ").appendDescriptionOf(' + variableName + ')';
+									}
+									
+									var describeToPostFix = ';\n' + TAB + '}';
+									return describeToPrefix + describeToBody + describeToPostFix;
+								})();
+		
+		var sectionClassSignatureClose = (function () {
+									return '}';
+								})();
+		
+		$scope.resultText = sectionPackage + '\n\n' + sectionImports + '\n\n' + sectionClassSignatureOpen + '\n\n' + sectionFields + sectionBuilderMethods + sectionMatchesSafely + '\n\n' + sectionDescribeTo +  '\n' + sectionClassSignatureClose;
+	}
+  }]);
